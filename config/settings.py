fa22f65410c3ed342928d462866550e19e8c7eee
MainUser <user@example.com>
2025-10-07 22:32:31 +0700

    устанвил приложения users, habits

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
+    "habits",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
