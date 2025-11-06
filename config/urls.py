4c47bdcf01135d9fd12c75d7b093fa5b66624439
MainUser <user@example.com>
2025-11-06 15:01:02 +0700

    добавил документацию

--- config/urls.py
+++ config/urls.py
@@ -35,4 +35,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
     path("api/", include("habits.urls", namespace="habits")),
+
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
