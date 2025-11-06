c5a60d7cd8955ae1bbe0c9653bd00674b7d507df
MainUser <user@example.com>
2025-11-06 08:11:45 +0700

    сделал разрешения и контроллеры привычек

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from .serializers import HabitSerializer
+from .permissions import IsOwner
+from .models import Habit
 
-# Create your views here.
+
+class HabitViewSet(viewsets.ModelViewSet):
+    """CRUD привычек текущего пользователя."""
+    serializer_class = HabitSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
+
+
+class PublicHabitListView(generics.ListAPIView):
+    """Список публичных привычек."""
+    serializer_class = HabitSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True)
