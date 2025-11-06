cc4447dfddd6bb3033b78950e4de26ee40d5df72
MainUser <user@example.com>
2025-11-06 08:05:37 +0700

    сделал контроллер юзера

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+
+class RegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (permissions.AllowAny,)
