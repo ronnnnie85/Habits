5251645569ea18855d03343a1d6ee74c22e70011
MainUser <user@example.com>
2025-11-16 21:50:39 +0700

    добавил секреты

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if "test" in sys.argv:
+if "tests" in sys.argv:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
